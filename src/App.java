e8ad785b42a641f4354bcef74b169f626073c472
hugohm <hugo2002p@outlook.com.br>
2021-06-15 22:13:50 -0300

    removidos import's não usados

--- src/App.java
+++ src/App.java
@@ -1,10 +1,3 @@
-import fila.FilaCaixa;
-import fila.SenhaCliente;
-
-import javax.swing.text.html.Option;
-import java.util.Optional;
-import java.util.Scanner;
-
 public class App {
 
     public static void main(String[] args) {
