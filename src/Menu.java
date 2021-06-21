a06f48beae3c1542664162d82094ccb56f638596
hugohm <hugo2002p@outlook.com.br>
2021-06-21 20:22:24 -0300

    adicionado alteração nas saídas de texto, para melhor formatação

--- src/Menu.java
+++ src/Menu.java
@@ -8,11 +8,14 @@ public class Menu {
 
     private Scanner scan = new Scanner(System.in);
 
+    String str = "=";
+    String repeated = str.repeat(35);
+
     public void init() {
         int op;
 
         while (true) {
-            System.out.print("+==================================+\n\t\t Menu de Opções\n\n Opção 1 - Chamar próximo Cliente\n Opção 2 - Retirar Nova Senha\n\n+==================================+\nDigite a opção: ");
+            System.out.print("+" + repeated + "+" + "\n\t\t Menu de Opções\n\n Opção 1 - Chamar próximo Cliente\n Opção 2 - Retirar Nova Senha\n\n" + "+" + repeated + "+\n" + "Digite a opção: ");
             op = scan.nextInt();
 
             if (op == 1) {
@@ -20,7 +23,7 @@ public class Menu {
             } else if (op == 2) {
                 retirarNovaSenha();
             } else {
-                System.out.println("\nOpção Fornecido Inválida, finalizando a sessão atual do programa.\n\n+==================================+");
+                System.out.println("\nOpção Fornecido Inválida, finalizando a sessão atual do programa.\n\n" + "+" + repeated + "+");
                 break;
             }
         }
@@ -29,14 +32,14 @@ public class Menu {
     public void proximo() {
         Optional<String> proximo = fila.chamarProximo();
         if (proximo.isPresent()) {
-            System.out.println("\nPróximo cliente da fila, senha: " + proximo.get() + ", se dirija ao caixa.\n\n+==================================+\n");
+            System.out.println("\nPróximo cliente da fila, senha: " + proximo.get() + ", se dirija ao caixa.\n\n" + "+" + repeated + "+" + "\n");
         } else {
-            System.out.println("\nNão tem mais nenhum cliente na fila, aguarde o próximo cliente chegar.\n\n+==================================+\n");
+            System.out.println("\nNão tem mais nenhum cliente na fila, aguarde o próximo cliente chegar.\n\n" + "+" + repeated + "+" + "\n");
         }
     }
 
     public void retirarNovaSenha() {
-        System.out.print("\n+==================================+\nTem Prioridade?\n\n1 - Sim\n2 - Não\n\n+==================================+\nDigite a opção: ");
+        System.out.print("\n+" + repeated + "+" + "\n\t\tTem Prioridade?\n\n1 - Sim\n2 - Não\n\n" + "+" + repeated + "+\n" + "Digite a opção: ");
 
         int priority = scan.nextInt();
         boolean hasPrioridade;
@@ -46,10 +49,10 @@ public class Menu {
         } else if (priority == 2) {
             hasPrioridade = false;
         } else {
-            System.out.println("\nOpção Fornecido Inválida, finalizando a sessão atual do programa.\n+==================================+\n");
+            System.out.println("\nOpção Fornecido Inválida, finalizando a sessão atual do programa.\n" + "+" + repeated + "+\n");
             return;
         }
 
-        System.out.println("\nSenha retirada: " + fila.adicionar(hasPrioridade) + "\n+==================================+\n");
+        System.out.println("\nSenha retirada: " + fila.adicionar(hasPrioridade) + "\n" + "+" + repeated + "+\n");
     }
 }
